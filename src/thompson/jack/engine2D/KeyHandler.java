8e797b2469861b25ac629da0a1ee291103312e43
JackCThompson <44539457+JackCThompson@users.noreply.github.com>
2019-04-04 14:15:36 -0700

    game exits when esc is pressed

--- src/thompson/jack/engine2D/KeyHandler.java
+++ src/thompson/jack/engine2D/KeyHandler.java
@@ -20,6 +20,10 @@ public class KeyHandler implements KeyListener{
 	}
 	
 	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			System.exit(0);
+		}
+		
 		keys[e.getKeyCode()] = true;
 	}
 
